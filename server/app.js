f02ad761e7838cf14fcd6f902d28502dd95839ef
Gerardo Barroso <gerardo_barroso@yahoo.com>
2018-12-04 22:55:33 -0800

    Add cors to server

--- server/app.js
+++ server/app.js
@@ -2,14 +2,18 @@ const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 
 const app = express()
+
+app.use(cors())
 
 mongoose.connect('mongodb://jerry:codehead666@ds119734.mlab.com:19734/graphql-app');
 mongoose.connection.once('open', () => {
 	console.log('connected to database')
 })
+
 
 app.use('/graphql', graphqlHTTP({
 	schema,

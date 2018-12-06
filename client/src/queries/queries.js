b0da13d969f71f9d3a6bc12f436dbb7dadc8b255
Gerardo Barroso <gerardo_barroso@yahoo.com>
2018-12-05 21:59:57 -0800

    Add external file queries

---
+++ client/src/queries/queries.js
@@ -0,0 +1,52 @@
+import { gql } from 'apollo-boost'
+
+const getBooksQuery = gql`
+    {
+        books {
+            name
+            genre
+            id
+        }
+    }
+`
+
+const getAuthorsQuery = gql`
+    {
+        authors {
+            name
+            id
+        }
+    }
+`
+
+const addBookMutation = gql`
+    mutation($name: String!, $genre: String!, $authorId:ID!){
+        addBook(name: $name, genre:$genre, authorId:$authorId){
+            name
+            id
+        }
+    }
+`
+
+const getBookQuery = gql`
+    query($id: ID){
+        book(id: $id){
+            name
+            genre
+            id
+            author{
+                id
+                name
+                age
+                books{
+                    name
+                    genre
+                    id
+                }
+            }
+        }
+    }
+
+`
+
+export { getAuthorsQuery, getBooksQuery, addBookMutation, getBookQuery }
